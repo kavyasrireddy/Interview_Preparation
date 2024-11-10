6e1ee663c045924da338c1e0b244596ad55e2567
kavyasrireddy <40555914+kavyasrireddy@users.noreply.github.com>
2024-11-10 21:31:48 +1100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,10 @@
-
+provider "aws"{
+region = "ap-south-1"
+}
+resource "aws_instance" "ec2_instance" {
+  ami             = "ami-08bf489a05e916bbd"
+  instance_type   = "t2.micro"
+  subnet_id       = "subnet-084d11669e62445d2"
+  security_groups = ["sg-0f45ab268d60539ce"]
+  keypair = "dockertest"
+}
